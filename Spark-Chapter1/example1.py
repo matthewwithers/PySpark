bf3f87350ef2393b03dbc996abb1b5c0e83c52b9
matthewwithers <matthew.withers3@loblaw.ca>
2021-06-08 16:35:21 -0400

    generic

--- Spark-Chapter1/example1.py
+++ Spark-Chapter1/example1.py
@@ -8,7 +8,30 @@ spark = SparkSession \
 flightData2015 = spark \
     .read \
     .option('inferSchema', 'True') \
-    .option('header','true') \
-    .csv('./Spark-The-Definitive-Guide/data/flight-data/csv/2015-summary.csv')
+    .option('header', 'true') \
+    .csv('../Spark-The-Definitive-Guide/data/flight-data/csv/2015-summary.csv')
 
 flightData2015.show()
+# print(flightData2015.take(3))
+# print(flightData2015.sort('count').explain())
+
+# DataFrames & Sql
+flightData2015.createOrReplaceTempView('flight_data_2015')
+
+sqlWay = spark.sql(
+    """
+    SELECT DEST_COUNTRY_NAME, count(1)
+    FROM flight_data_2015
+    GROUP BY DEST_COUNTRY_NAME
+    """
+)
+
+dataFrameWay = flightData2015 \
+    .groupBy('DEST_COUNTRY_NAME') \
+    .count()
+
+sqlWay.show()
+sqlWay.explain()
+
+dataFrameWay.show()
+dataFrameWay.explain()
