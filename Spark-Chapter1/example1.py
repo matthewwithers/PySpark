e55b0dea33237f52dab0be4d8b4ff2015310e372
matthewwithers <matthew.withers3@loblaw.ca>
2021-06-08 20:20:42 -0400

    generic

--- Spark-Chapter1/example1.py
+++ Spark-Chapter1/example1.py
@@ -1,14 +1,15 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import max, desc
 
-spark = SparkSession \
-    .builder \
-    .appName('Sample App') \
+spark = SparkSession\
+    .builder\
+    .appName('Sample App')\
     .getOrCreate() 
 
-flightData2015 = spark \
-    .read \
-    .option('inferSchema', 'True') \
-    .option('header', 'true') \
+flightData2015 = spark\
+    .read\
+    .option('inferSchema', 'True')\
+    .option('header', 'true')\
     .csv('../Spark-The-Definitive-Guide/data/flight-data/csv/2015-summary.csv')
 
 flightData2015.show()
@@ -20,7 +21,8 @@ flightData2015.createOrReplaceTempView('flight_data_2015')
 
 sqlWay = spark.sql(
     """
-    SELECT DEST_COUNTRY_NAME, count(1)
+    SELECT 
+        DEST_COUNTRY_NAME, count(1)
     FROM flight_data_2015
     GROUP BY DEST_COUNTRY_NAME
     """
@@ -35,3 +37,27 @@ sqlWay.explain()
 
 dataFrameWay.show()
 dataFrameWay.explain()
+
+# Get the max values in the DataFrame
+flightData2015 \
+    .select(max('count')).take(1)
+
+maxSql = spark.sql(
+    """
+    SELECT DEST_COUNTRY_NAME, sum(count) as destination_total
+    FROM flight_data_2015
+    GROUP BY DEST_COUNTRY_NAME
+    ORDER BY sum(count) DESC
+    """
+)
+
+maxSql.show()
+
+
+flightData2015\
+    .groupBy('DEST_COUNTRY_NAME')\
+    .sum('count')\
+    .withColumnRenamed('sum(count)','destination_total')\
+    .sort(desc("destination_total"))\
+    .limit(5)\
+    .show()
