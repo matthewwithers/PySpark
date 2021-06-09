d064640cc57d3fef7825e29bd10fad803cf5cc4f
matthewwithers <matthew.withers3@loblaw.ca>
2021-06-09 13:39:18 -0400

    Dataframes and Datasets

--- Structured API/dataframes_datasets_p1.py
+++ Structured API/dataframes_datasets_p1.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
 
 spark = SparkSession\
     .builder\
@@ -20,4 +21,27 @@ that are manipulated with transformations and actions are Dataframes and Dataset
 To create a new DataFrame or Dataset you call a transformation.
 To start a computation or covert to native language types you call an action.
 """
+
+if __name__ == '__main__':
+
+
+    print(spark.range(2).collect())
+    # Creates an array of type Row(): [Row(id=0), Row(id=1)]
+
+    df = spark\
+        .read\
+        .format('json')\
+        .load('../Spark-The-Definitive-Guide/data/flight-data/json/2015-summary.json')
+    
+
+    df.printSchema()
+    """
+    root
+        |-- DEST_COUNTRY_NAME: string (nullable = true)
+        |-- ORIGIN_COUNTRY_NAME: string (nullable = true)
+        |-- count: long (nullable = true)
+    """
+
+
+
 
