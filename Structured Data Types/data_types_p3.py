0f36dc2a7a49e47d14cddfcdbcbe4a1307a92323
matthewwithers <matthew.withers3@loblaw.ca>
2021-06-23 10:31:32 -0400

    commit

--- Structured Data Types/data_types_p3.py
+++ Structured Data Types/data_types_p3.py
@@ -54,3 +54,44 @@ df\
         col('description')
     )\
     .show(2)
+
+extract_str = '(BLACK|WHITE|RED|GREEN|BLUE)'
+df\
+    .select(
+        regexp_extract(col('description'), extract_str,
+                       1).alias('extacted_str'),
+        col('description')
+    ).show(5)
+
+# In Spark we can also check from String that contain certain substrings
+containsBlack = instr(col('description'), 'BLACK') >= 1
+containsWhite = instr(col('description'), 'WHITE') >= 1
+
+df\
+    .withColumn('hasSimpleColor', containsBlack | containsWhite)\
+    .where('hasSimpleColor')\
+    .select('description', 'hasSimpleColor')\
+    .show(5, False)
+
+simpleColors = ['black', 'white', 'red', 'green', 'blue']
+
+
+def color_locator(column, color_string):
+    return locate(color_string.upper(), column)\
+        .cast('boolean')\
+        .alias('is_' + color_string)
+
+
+selectedColumns = [color_locator(df.Description, color)
+                   for color in simpleColors]
+selectedColumns.append(expr('*'))  # has to be a Column type
+
+
+df\
+    .select(*selectedColumns).show(3)
+
+df\
+    .select(*selectedColumns)\
+    .where(expr('is_white OR is_red'))\
+    .select('description')\
+    .show(3, False)
